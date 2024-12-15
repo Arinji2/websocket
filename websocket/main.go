be840650cada5f2b11ef91f097e318119173ccb4
Arinji2 <arinjaydhar205@gmail.com>
2024-12-15 22:42:45 +0530

    Finish Pinging and Continous Pinging

--- websocket/main.go
+++ websocket/main.go
@@ -1,8 +1,12 @@
 package socket
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,8 +22,62 @@ func (wsh WebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
+	cx, cancel := context.WithCancel(r.Context())
+
 	defer func() {
 		fmt.Println("Closing Connection")
 		c.Close()
+		cancel()
 	}()
+
+	err = c.WriteMessage(websocket.TextMessage, []byte("connected to server successfully"))
+	if err != nil {
+		log.Printf("Error sending message %v", err)
+		return
+	}
+	for {
+		messageType, message, err := c.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message %v", err)
+			return
+		}
+		if messageType == websocket.BinaryMessage {
+			err = c.WriteMessage(websocket.TextMessage, []byte("server does not process binary data"))
+			if err != nil {
+				log.Printf("Error sending message %v", err)
+			}
+		}
+
+		trimmedMessage := strings.Trim(string(message), "\n")
+		if trimmedMessage == "ping" {
+			fmt.Println("Server Pinged")
+			c.WriteMessage(websocket.TextMessage, []byte("SERVER: pong"))
+		}
+
+		if trimmedMessage == "start" {
+			go func(ctx context.Context) {
+				i := 1
+				c.WriteMessage(websocket.TextMessage, []byte("SERVER: Starting Continuous Pinging"))
+				for {
+					select {
+					case <-cx.Done():
+					default:
+						err := c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("SERVER: ping x%d", i)))
+						if err != nil {
+							log.Printf("Error sending message %v", err)
+							return
+						}
+					}
+					time.Sleep(2 * time.Second)
+					i++
+				}
+			}(cx)
+		}
+
+		if trimmedMessage == "stop" {
+			fmt.Println("Stopping Pinging")
+			cancel()
+			c.WriteMessage(websocket.TextMessage, []byte("SERVER: Stopping Pinging"))
+		}
+	}
 }
